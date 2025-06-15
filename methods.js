5a8b216f856c7110bc6c7958d919a78f5b812922
yamauchi <ev.takanori.yamauchi@gmail.com>
2025-06-16 06:59:07 +0900

    とりあえずうｐ

---
+++ methods.js
@@ -0,0 +1,12 @@
+const jsonData = [
+  {
+    stored_name: "stored_method1",
+    description: "table1のculum1にargを更新する",
+    output_color: "#007acc",
+  },
+  {
+    stored_name: "stored_method2",
+    description: "table2のculum2にargを更新する",
+    output_color: "#cc0000",
+  },
+];
